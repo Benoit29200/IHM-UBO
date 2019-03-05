8ea9a03301adce296a20ac8c7c4d023da2e29967
GaetanCa <gaetan.caignard@hotmail.com>
2019-03-05 17:18:49 +0100

    CreationCompte

--- TwitUp/src/com/iup/tp/twitup/ihm/TwitupCreationCompte.java
+++ TwitUp/src/com/iup/tp/twitup/ihm/TwitupCreationCompte.java
@@ -9,6 +9,7 @@ import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
@@ -24,89 +25,53 @@ public class TwitupCreationCompte extends JPanel{
 		
 		create.setLayout(new GridBagLayout());
 		
-		JPanel creationLabel = createJPanelLabel("Votre nom :");
-		create.setBorder(new LineBorder(Color.RED, 4, true));
-		
-		JPanel creationText = createJPanelTextField(screenSize);
-		create.setBorder(new LineBorder(Color.RED, 4, true));
-		
-		JPanel creationLabel1 = createJPanelLabel("Votre nom :");
-		create.setBorder(new LineBorder(Color.RED, 4, true));
-		
-		JPanel creationText1 = createJPanelTextField(screenSize);
-		create.setBorder(new LineBorder(Color.RED, 4, true));
-		
-		create.add(creationLabel, new GridBagConstraints(0, 0, 2, 1, 1, 1, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
-
-		create.add(creationText, new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHEAST,
-				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-		
-		create.add(creationLabel1, new GridBagConstraints(0, 1, 2, 1, 1, 1, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
-
-		create.add(creationText1, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.NORTHEAST,
-				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		createUserPage(create);
 		
 		create.setBorder(new LineBorder(Color.CYAN, 4, true));
-		create.setPreferredSize(new Dimension(screenSize.width/3, screenSize.height/2));
+		create.setPreferredSize(new Dimension((int) (screenSize.width/2.5), screenSize.height/2));
 		create.setOpaque(true);
 		add(create);
 		
 	}
-		
-		/*setLayout(new BorderLayout());
-		
-		Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		
-		JPanel userCreationLabel = setCreationPanelLabel(screenSize);
-		JPanel userCreationTextField = setCreationPanelTextField(screenSize);
-		
-		JPanel create = new JPanel();
-		create.setLayout(new GridLayout(1, 2));
-		create.setBorder(BorderFactory.createTitledBorder("Vive le roi Benoît"));
-		create.setPreferredSize(new Dimension(screenSize.width/2, screenSize.height/2));
-		create.add(userCreationLabel);
-		create.add(userCreationTextField);
-		
 
-	    JPanel creation = new JPanel();
-	    creation.add(creationcompteJButton);
-	    add(creation, BorderLayout.SOUTH);
+	private void createUserPage(JPanel create) {
+		JPanel creationLabel = createJPanelLabel("Votre nom :");
+		create.setBorder(new LineBorder(Color.RED, 4, true));
+		JPanel creationText = createJPanelTextField(screenSize,false);
+		create.setBorder(new LineBorder(Color.RED, 4, true));
+		JPanel creationLabel1 = createJPanelLabel("Votre pseudo :");
+		create.setBorder(new LineBorder(Color.RED, 4, true));
+		JPanel creationText1 = createJPanelTextField(screenSize,false);
+		create.setBorder(new LineBorder(Color.RED, 4, true));
+		JPanel creationLabel2 = createJPanelLabel("Votre mot de passe :");
+		create.setBorder(new LineBorder(Color.RED, 4, true));
+		JPanel creationText2 = createJPanelTextField(screenSize,true);
+		create.setBorder(new LineBorder(Color.RED, 4, true));
+		JPanel creationLabel3 = createJPanelLabel("Confirmation mot de passe :");
+		create.setBorder(new LineBorder(Color.RED, 4, true));
+		JPanel creationText3 = createJPanelTextField(screenSize,true);
+		create.setBorder(new LineBorder(Color.RED, 4, true));
 		
-	    add(create,BorderLayout.CENTER);
+		create.add(creationLabel, new GridBagConstraints(0, 0, 2, 1, 1, 1, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
+		create.add(creationText, new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHEAST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		create.add(creationLabel1, new GridBagConstraints(0, 1, 2, 1, 1, 1, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
+		create.add(creationText1, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.NORTHEAST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		create.add(creationLabel2, new GridBagConstraints(0, 2, 2, 1, 1, 1, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
+		create.add(creationText2, new GridBagConstraints(0, 2, 1, 1, 1, 1, GridBagConstraints.NORTHEAST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		create.add(creationLabel3, new GridBagConstraints(0, 3, 2, 1, 1, 1, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
+		create.add(creationText3, new GridBagConstraints(0, 3, 1, 1, 1, 1, GridBagConstraints.NORTHEAST,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		create.add(this.creationcompteJButton, new GridBagConstraints(0, 4, 1, 1, 1, 1, GridBagConstraints.SOUTH,
+				GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
 	}
-
-	private JPanel setCreationPanelLabel(Dimension screenSize) {
-		JPanel userCreation = new JPanel();
-		userCreation.setPreferredSize(new Dimension(screenSize.width/4, screenSize.height/2));
-		userCreation.setLayout(new GridLayout(5,1));
-		JPanel creation = createJPanelLabel("Votre nom :");
-		userCreation.add(creation);
-		creation = createJPanelLabel("Votre pseudo:");
-		userCreation.add(creation);
-		creation = createJPanelLabel("Votre mot de passe :");
-		userCreation.add(creation);
-		creation = createJPanelLabel("Confirmation mot de passe :");
-		userCreation.add(creation);
-		return userCreation;
-	}
-	
-	private JPanel setCreationPanelTextField(Dimension screenSize) {
-		JPanel userCreation = new JPanel();
-		userCreation.setPreferredSize(new Dimension(screenSize.width/4, screenSize.height/2));
-		userCreation.setLayout(new GridLayout(5,1));
-		JPanel creation = createJPanelTextField(screenSize);
-		userCreation.add(creation);
-		creation = createJPanelTextField(screenSize);
-		userCreation.add(creation);
-		creation = createJPanelTextField(screenSize);
-		userCreation.add(creation);
-		creation = createJPanelTextField(screenSize);
-		userCreation.add(creation);
-		return userCreation;
-	}
-*/
+		
 	private JPanel createJPanelLabel(String titreLabel) {
 		JPanel creation = new JPanel();
 		JLabel nom = new JLabel(titreLabel);
@@ -114,11 +79,18 @@ public class TwitupCreationCompte extends JPanel{
 		return creation;
 	}
 	
-	private JPanel createJPanelTextField(Dimension screenSize) {
+	private JPanel createJPanelTextField(Dimension screenSize,Boolean pwd) {
 		JPanel creation = new JPanel();
-		JTextField nomText = new JTextField("");
-		nomText.setPreferredSize(new Dimension(screenSize.width/5, screenSize.height/22));
-		creation.add(nomText);
+		if(pwd) {
+			JPasswordField monText = new JPasswordField();
+			monText.setPreferredSize(new Dimension(screenSize.width/5, screenSize.height/22));
+			creation.add(monText);
+		}
+		else {
+			JTextField nomText = new JTextField("");
+			nomText.setPreferredSize(new Dimension(screenSize.width/5, screenSize.height/22));
+			creation.add(nomText);
+		}
 		return creation;
 	}
 
