cf4134abfd322a04b96e249b972b095d034c7836
GaetanCa <gaetan.caignard@hotmail.com>
2019-03-08 11:26:58 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Benoit29200/IHM-UBO.git""
    This reverts commit fbed628dcd9ac34b8820d35b744d0d289c2d11f7.

--- TwitUp/src/com/iup/tp/twitup/common/Constants.java
+++ TwitUp/src/com/iup/tp/twitup/common/Constants.java
@@ -217,4 +217,9 @@ public interface Constants {
 	 * Clé pour la liste des users
 	 */
 	public static final String USER_LIST_USERS = "listeuser";
+	
+	/**
+	 * Clé pour voir l'utilisateur
+	 */
+	public static final String USER_SEE_OTHER_USER = "voiruser";
 }
