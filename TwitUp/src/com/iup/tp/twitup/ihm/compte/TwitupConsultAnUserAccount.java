7bef1ccffbbb3093e3f05e4d50c4d87b39ff29ed
GaetanCa <gaetan.caignard@hotmail.com>
2019-03-11 17:24:24 +0100

    Change

--- TwitUp/src/com/iup/tp/twitup/ihm/compte/TwitupConsultAnUserAccount.java
+++ TwitUp/src/com/iup/tp/twitup/ihm/compte/TwitupConsultAnUserAccount.java
@@ -8,14 +8,11 @@ import java.awt.Insets;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
-import javax.swing.border.TitledBorder;
+import javax.swing.border.LineBorder;
 
 import com.iup.tp.twitup.common.Constants;
 import com.iup.tp.twitup.communicationInterface.vueController.consultAccountUser.IObservableConsultAccountUser;
@@ -53,6 +50,7 @@ public class TwitupConsultAnUserAccount extends JPanel implements IObservableCon
 		this.addObserver(observer);
 		this.user = theUser;
 		this.initComponent();
+		this.setLayout(new GridBagLayout());
 
 		if(userConnected.getFollows().contains(theUser.getUserTag())) {
 			connectToThisAccount = new JButton(this.fileLanguage.getObject(Constants.DECONNECTION_TO_ACCOUNT).toString());
@@ -64,16 +62,15 @@ public class TwitupConsultAnUserAccount extends JPanel implements IObservableCon
 		}
 		this.user = theUser;
 		this.avatar = new JLabel(new ImageIcon(getClass().getResource(theUser.getAvatarPath())));
-		Border compound = null;
-		this.setBorder(BorderFactory.createTitledBorder(compound, "",TitledBorder.CENTER, TitledBorder.BELOW_BOTTOM));
-		this.setBackground(Color.WHITE);
-		this.add(setPanelAccount());
+		this.setBackground(new Color(135,206,250));
+		this.add(setPanelAccount(),new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
 
 	}
 
 	private void initComponent(){
 		this.screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		this.tailleComponent = new Dimension(screenSize.width/3, screenSize.height/15);
+		this.tailleComponent = new Dimension(screenSize.width/6, screenSize.height/15);
 
 		this.nomLabel = new JLabel(this.fileLanguage.getObject(Constants.VIEW_NOM).toString());
 		this.nom = new JLabel(this.user.getName());
@@ -95,12 +92,15 @@ public class TwitupConsultAnUserAccount extends JPanel implements IObservableCon
 	 */
 	private JPanel setPanelAccount() {
 		JPanel panelResearchTwit = new JPanel( new GridBagLayout());
-		panelResearchTwit.add(avatar,new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST,
+		panelResearchTwit.add(avatar,new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
 				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
 		panelResearchTwit.add(this.setLabelPanel(),new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER,
 				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
-		panelResearchTwit.add(this.setTextFieldPanel(),new GridBagConstraints(1, 1, 1, 1, 1, 1, GridBagConstraints.CENTER,
+		panelResearchTwit.add(this.setTextFieldPanel(),new GridBagConstraints(0, 2, 1, 1, 1, 1, GridBagConstraints.CENTER,
 				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
+		panelResearchTwit.add(connectToThisAccount,new GridBagConstraints(0, 3, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
+		panelResearchTwit.setBorder(new LineBorder(Color.white,4));
 		return panelResearchTwit;
 	}
 	
@@ -111,9 +111,9 @@ public class TwitupConsultAnUserAccount extends JPanel implements IObservableCon
 	private JPanel setLabelPanel() {
 		JPanel panelLabelUserAccount = new JPanel(new GridBagLayout());
 
-		panelLabelUserAccount.add(nomLabel,new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER,
+		panelLabelUserAccount.add(nomLabel,new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.EAST,
 				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
-		panelLabelUserAccount.add(pseudoLabel,new GridBagConstraints(0,2, 1, 1, 1, 1, GridBagConstraints.CENTER,
+		panelLabelUserAccount.add(nom,new GridBagConstraints(1, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
 				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
 		return panelLabelUserAccount;
 	}
@@ -125,11 +125,9 @@ public class TwitupConsultAnUserAccount extends JPanel implements IObservableCon
 	private JPanel setTextFieldPanel() {
 		JPanel panelTextFieldUserAccount = new JPanel(new GridBagLayout());
 		
-		panelTextFieldUserAccount.add(nom,new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
+		panelTextFieldUserAccount.add(pseudoLabel,new GridBagConstraints(0,0, 1, 1, 1, 1, GridBagConstraints.EAST,
 				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
-		panelTextFieldUserAccount.add(pseudo,new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER,
-				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
-		panelTextFieldUserAccount.add(connectToThisAccount,new GridBagConstraints(0, 2, 1, 1, 1, 1, GridBagConstraints.CENTER,
+		panelTextFieldUserAccount.add(pseudo,new GridBagConstraints(1, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
 				GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
 		
 		return panelTextFieldUserAccount;
